cf304d917f376c1a44390a6210e84b9b5c553cc9
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-11 15:35:36 -0500

    Challenge 1

--- class2.js
+++ class2.js
@@ -14,3 +14,5 @@ var fullName_new = `${name} ${lastname}`
 var fullName_jsInside = `${name} ${lastname.toUpperCase()}`
 
 var str = name.substr(1, 2)//name.charAt(1) + name.charAt(2)
+
+var lastNameLetter = name.charAt(name.length-1)
