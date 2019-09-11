bfc3cdce64f3db9c3f16cd0dcb5fe5672b1fc945
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-11 15:33:07 -0500

    Strings variables

---
+++ class2.js
@@ -0,0 +1,16 @@
+var name = 'César', lastname = 'Dueñas'
+
+var nameUppercase = name.toUpperCase()
+var lastnameLowercase = lastname.toLowerCase()
+
+var firstNameLetter = name.charAt(0)
+
+var amountLetterInName = name.length
+
+var fullName = name + ' ' + lastname
+
+var fullName_new = `${name} ${lastname}`
+
+var fullName_jsInside = `${name} ${lastname.toUpperCase()}`
+
+var str = name.substr(1, 2)//name.charAt(1) + name.charAt(2)
