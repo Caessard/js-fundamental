42b5209d02272df5293cadd2d8520ec51bdb0274
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-11 15:46:05 -0500

    Numbers as variables

---
+++ class3.js
@@ -0,0 +1,21 @@
+var age = 28
+
+//age = age + 1
+age += 1
+
+var weight = 73
+
+//weight = weight - 2
+weight -= 2
+
+var sadwich = 1
+
+weight += sadwich
+
+var playSoccer = 3
+
+weight -= playSoccer
+
+var winePrice = 200.3
+
+var total = winePrice * 3
