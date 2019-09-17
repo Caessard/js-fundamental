9221524ed6384f9d4812a4353397d11c2ce0912a
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-17 15:24:15 -0500

    Get restult in succes

--- class_callbacks.js
+++ class_callbacks.js
@@ -4,7 +4,7 @@ const PEOPLE_URL = 'people/:id'
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const options = { crossDomain: true }
 
-$.get(lukeUrl, options, function(){
-    console.log(arguments)
+$.get(lukeUrl, options, function(luke){
+    console.log(`Hola, yo soy ${luke.name}`)
 })
 
