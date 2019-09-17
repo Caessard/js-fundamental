b4be9c45f1bbb4ed2b74bc50c12d98ec264fd825
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-17 13:49:24 -0500

    More calls to print function

--- class4.js
+++ class4.js
@@ -2,8 +2,11 @@
 
 var name = 'César', age = 28
 
-function printAge() {
-    console.log (`${name} tiene ${age} años.`)
+function printAge(n, a) {
+    console.log (`${n} tiene ${a} años.`)
 }
 
-printAge()
+printAge(name, age)
+printAge('Vicky', 24)
+printAge('Eric', 32)
+printAge('Carmen', 27)
