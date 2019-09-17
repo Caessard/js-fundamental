28a21ab28407d639d0395863de25e4a20d9c072f
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-17 15:32:22 -0500

    Get first 100 characters names

--- class_callbacks.js
+++ class_callbacks.js
@@ -1,12 +1,18 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const options = { crossDomain: true }
 
 const onPeopleResponse = function(person){
     console.log(`Hola, yo soy ${person.name}`)
 }
 
-$.get(lukeUrl, options, onPeopleResponse)
+function getCharacter(id){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get(url, options, onPeopleResponse)
+}
+
+for(var i = 1; i==100; i++){
+    getCharacter(i)
+}
 
